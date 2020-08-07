af9327a1ef507b552f31ae2cecb915fc4c39970c
Kemalele <kemalele17@gmail.com>
2020-08-07 16:59:48 +0600

    set up Post models

--- kodlandblog/settings.py
+++ kodlandblog/settings.py
@@ -103,6 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+class Post(models.Model):
+    name =  models.CharField(max_length=30)
+    description = models.TextField()
+    pub_date = models.DateTimeField(default=now, editable=False)
+    image = models.ImageField(upload_to='userImages')
+
