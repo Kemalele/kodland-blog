097c499601e5e50c0024e1c5030654ddd36fd150
Kemalele <kemalele17@gmail.com>
2020-08-07 17:26:42 +0600

    added routing

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("You're on main page")
+
+def detail(request,post_id):
+    return HttpResponse("You're looking at post %s."% post_id)
+
+def create(request):
+    return HttpResponse("You're creating new post")
